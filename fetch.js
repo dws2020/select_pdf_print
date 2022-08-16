5061b09da002901a5dfef6eb0fd63e139f9c8d27
dws2020 <73006643+dws2020@users.noreply.github.com>
2022-08-16 19:14:24 +0900

    refactoring

--- fetch.js
+++ fetch.js
@@ -1,47 +1,28 @@
-!function(){
-    let url = 'doc.json';
-    async function fetchFiles() {
-        let res   = await fetch(url);
-        let files = await res.json();  
-
+import {pdfs} from "./pdfs.js";
+    function fetchPdfs() {
         let secSheets1 = document.getElementById('sheets-1');
-        for (let i = 0; i <= files.length - 1; i++) {
+        for (let i = 0; i <= pdfs.length - 1; i++) {
             const divTag    = document.createElement('div');
             const inputTag  = document.createElement('input');
-            const labelTag      = document.createElement('label');
+            const labelTag  = document.createElement('label');
             
             divTag.classList.add('select-btn');
-            divTag.id      = 'div-' + files[i].fileName;
+            divTag.id      = 'div-' + pdfs[i].fileName;
 
             inputTag.classList.add('inputs');
             inputTag.type  = 'checkbox';
-            inputTag.id    = 'input-' + files[i].fileName;
-            if(files[i].default === "on") {
+            inputTag.id    = 'input-' + pdfs[i].fileName;
+            if(pdfs[i].default === "on") {
                 inputTag.checked = true;
             }
 
             labelTag.classList.add('select-btn-label');
-            labelTag.id        = 'label-' + files[i].fileName;
-            labelTag.innerText = files[i].docName;
-            labelTag.htmlFor   =  'input-' + files[i].fileName;
+            labelTag.id        = 'label-' + pdfs[i].fileName;
+            labelTag.innerText = pdfs[i].docName;
+            labelTag.htmlFor   =  'input-' + pdfs[i].fileName;
 
             divTag.append(inputTag, labelTag);
             secSheets1.append(divTag);
         }
-        // let inputTags = document.querySelectorAll('.inputs');
-
-        // let docNames = document.querySelectorAll('.select-btn-p');
-        // docNames.forEach((docName)=> {
-        //     docName.addEventListener('click', () => {
-        //     let fileName = docName.id.replace('p-','');
-        //     let targetInput = document.getElementById('input-' + fileName);
-        //     if (targetInput.checked === true) {
-        //         targetInput.checked = false;
-        //     } else {
-        //         targetInput.checked = true;
-        //     }
-        //     });
-        // })
     }
-    fetchFiles();
-}();
+    fetchPdfs();
